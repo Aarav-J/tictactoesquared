a98123201e1f081133b40e4022a7cccfd7ad9d0c
Som Shrivastava <som.shrivastava@gmail.com>
2023-08-16 23:09:31 -0400

    created hero section of landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.scss";
 import CodeConnectLogo from "./assets/logo.svg";
+import HeroSvg from "./assets/hero.svg";
 
 function App() {
 
@@ -15,6 +16,20 @@ function App() {
           <a className="navbar__links__link"> Get Involved </a>
         </div>
         <button className="navbar__contact-button">Contact Us</button>
+      </section>
+      <section className="hero">
+        <div className="hero__content">
+          <h1 className="hero__content__title">
+            Bringing
+            <span className="hero__content__title--accent"> computer science </span>
+            to K-12 students.
+          </h1>
+          <p className="hero__content__paragraph">
+            We foster development in the computer science community through hosting a variety of interactive programs.
+          </p>
+          <button className="hero__content__programs-button">Programs</button>
+        </div>
+        <img className="hero__image" src={HeroSvg} alt="Hero SVG" />
       </section>
     </>
   );
